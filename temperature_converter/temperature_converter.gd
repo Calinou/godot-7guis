fa508f35f58a22569d2d322f4cdf1834481be744
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-05-10 18:56:42 +0200

    Add a temperature converter demo
    See https://eugenkiss.github.io/7guis/tasks#temp.

---
+++ temperature_converter/temperature_converter.gd
@@ -0,0 +1,37 @@
+# Copyright © 2019 Hugo Locurcio and contributors - MIT License
+# See `LICENSE.md` included in the source distribution for details.
+
+extends HBoxContainer
+
+onready var celsius_line_edit := $CelsiusLineEdit as LineEdit
+onready var fahrenheit_line_edit := $FahrenheitLineEdit as LineEdit
+
+var celsius := 0.0 setget set_celsius
+var fahrenheit := 32.0 setget set_fahrenheit
+
+func _ready() -> void:
+	OS.window_size = Vector2(320, 60)
+
+func _on_celsius_text_changed(new_text: String) -> void:
+	self.celsius = float(new_text)
+
+	# Empty the other field if this field is empty
+	if new_text == "":
+		fahrenheit_line_edit.text = ""
+
+func _on_fahrenheit_text_changed(new_text: String) -> void:
+	self.fahrenheit = float(new_text)
+
+	# Empty the other field if this field is empty
+	if new_text == "":
+		celsius_line_edit.text = ""
+
+func set_celsius(p_celsius: float) -> void:
+	fahrenheit = p_celsius * 1.8 + 32
+	fahrenheit_line_edit.text = str(fahrenheit)
+	celsius = p_celsius
+
+func set_fahrenheit(p_fahrenheit: float) -> void:
+	celsius = (p_fahrenheit - 32) / 1.8
+	celsius_line_edit.text = str(celsius)
+	fahrenheit = p_fahrenheit
