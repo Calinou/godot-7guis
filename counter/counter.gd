877dff7cf7fa7a940f6085cf31273f1ef60306ba
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-05-10 18:55:33 +0200

    counter: Set a small initial window size

--- counter/counter.gd
+++ counter/counter.gd
@@ -1,3 +1,6 @@
+# Copyright © 2019 Hugo Locurcio and contributors - MIT License
+# See `LICENSE.md` included in the source distribution for details.
+
 extends HBoxContainer
 
 # This caches the `get_node()` call and infers the type of the variable
@@ -6,10 +9,13 @@ onready var line_edit := $LineEdit as LineEdit
 
 var counter := 0 setget set_counter
 
-func set_counter(p_counter: int) -> void:
-	line_edit.text = str(p_counter)
-	counter = p_counter
+func _ready() -> void:
+	OS.window_size = Vector2(240, 60)
 
 func _on_button_pressed() -> void:
 	# `self` is required here, so that it calls the setter method defined above
 	self.counter += 1
+
+func set_counter(p_counter: int) -> void:
+	line_edit.text = str(p_counter)
+	counter = p_counter
